5387d7c21f0e3aa9cea3c7200a6bf06e5593ecaa
aquarius-kuchain <aquarius@kuchain.io>
2020-07-31 12:49:18 +0800

    change main symbol to kuchain

--- chain/constants/keys/keys.go
+++ chain/constants/keys/keys.go
@@ -2,7 +2,7 @@ package keys
 
 const (
 	ChainNameStr      = "sys"
-	ChainMainNameStr  = "chain"
+	ChainMainNameStr  = "kuchain"
 	DefaultBondSymbol = ChainNameStr
 	DefaultBondDenom  = ChainMainNameStr + "/" + DefaultBondSymbol
 )
--- scripts/boot-testnet.sh
+++ scripts/boot-testnet.sh
@@ -12,14 +12,14 @@ mkdir -p ${ROOT_DIR}
 mkdir -p ${ROOT_DIR}/node/config
 mkdir -p ${ROOT_DIR}/cli/
 
-printf "start testnet for chain ${CHAIN_ID} in ${ROOT_DIR}\\n"
+printf "start testnet for kuchain ${CHAIN_ID} in ${ROOT_DIR}\\n"
 
 PARAMS="--home ${ROOT_DIR}/node/"
 PARAMSCLI="--home ${ROOT_DIR}/cli/ --keyring-backend test"
 
-./build/kucli ${PARAMSCLI} keys add chain
+./build/kucli ${PARAMSCLI} keys add kuchain
 
-VALKEY=$(./build/kucli ${PARAMSCLI} keys show chain -a)
+VALKEY=$(./build/kucli ${PARAMSCLI} keys show kuchain -a)
 
 ./build/kucli ${PARAMSCLI} keys add test
 
@@ -28,17 +28,17 @@ TESTVALKEY=$(./build/kucli ${PARAMSCLI} keys show test -a)
 printf "current val key ${VALKEY}\\n"
 
 ./build/kucd init ${PARAMS} --chain-id=${CHAIN_ID} ${CHAIN_ID}
-./build/kucd ${PARAMS} genesis add-account chain ${VALKEY}
+./build/kucd ${PARAMS} genesis add-account kuchain ${VALKEY}
 ./build/kucd ${PARAMS} genesis add-account testacc1 ${TESTVALKEY}
 ./build/kucd ${PARAMS} genesis add-account testacc2 ${TESTVALKEY}
 ./build/kucd ${PARAMS} genesis add-address ${VALKEY}
-./build/kucd ${PARAMS} genesis add-coin "1000000000000000000000000000000000000000chain/sys" "main token"
+./build/kucd ${PARAMS} genesis add-coin "1000000000000000000000000000000000000000kuchain/sys" "main token"
 #./build/kucd ${PARAMS} genesis add-coin "1000000000000000000000000000000000000000validatortoken" "for staking"
-./build/kucd ${PARAMS} genesis add-account-coin ${VALKEY} "100000000000000000000000chain/sys"
-./build/kucd ${PARAMS} genesis add-account-coin chain "100000000000000000000000chain/sys"
+./build/kucd ${PARAMS} genesis add-account-coin ${VALKEY} "100000000000000000000000kuchain/sys"
+./build/kucd ${PARAMS} genesis add-account-coin kuchain "100000000000000000000000kuchain/sys"
 
-printf "./build/kucd ${PARAMS} gentx ${VALKEY} --keyring-backend test --name chain --home-client ${ROOT_DIR}/cli/\\n"
-./build/kucd ${PARAMS} gentx ${VALKEY} --keyring-backend test --name chain --home-client ${ROOT_DIR}/cli/
+printf "./build/kucd ${PARAMS} gentx ${VALKEY} --keyring-backend test --name kuchain --home-client ${ROOT_DIR}/cli/\\n"
+./build/kucd ${PARAMS} gentx ${VALKEY} --keyring-backend test --name kuchain --home-client ${ROOT_DIR}/cli/
 
 
 ./build/kucd ${PARAMS} collect-gentxs
